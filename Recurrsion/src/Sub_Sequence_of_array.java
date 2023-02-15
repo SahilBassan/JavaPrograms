0fee79b9011fe2cdc9caa8b0347d6534819733d6
sam02202001 <sahil2001bassan@gmail.com>
2023-02-16 01:43:01 +0530

    Initial version

---
+++ Recurrsion/src/Sub_Sequence_of_array.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Sub_Sequence_of_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length of array ; ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter all the elements of array separated by space :");
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        subSequenceOfArray(new ArrayList<>(),0, arr, n);
+    }
+
+    public static void subSequenceOfArray(ArrayList<Integer> arrl, int index, int[] arr, int n){
+        //base case
+        if(index == n){
+            System.out.println(arrl);
+            return;
+        }
+
+        //Select side of tree first
+        arrl.add(arr[index]);
+        subSequenceOfArray(arrl, index+1, arr, n);
+
+        arrl.remove(arrl.size() - 1);
+        subSequenceOfArray(arrl, index+1, arr, n);
+
+        // Not select side of tree first
+//        subSequenceOfArray(arrl, index+1, arr, n);
+//
+//        arrl.add(arr[index]);
+//        subSequenceOfArray(arrl, index+1, arr, n);
+//
+//        arrl.remove(arrl.size() - 1);
+    }
+}
